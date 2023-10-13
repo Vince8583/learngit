e6c7095f6b246340154b7a8b1aa9228ea0c0058a
Vince <1223740104@qq.com>
2023-10-13 10:48:17 +0800

    C文件修改

---
+++ git_learning/git_learning/test.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int ans;
+	int maxn;
+	return 0;
+}
--- git_learning/git_learning/源.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	int ans;
-	int maxn;
-	return 0;
-}
